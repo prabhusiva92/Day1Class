2c69d4b60c1bf11200d2817ecd60187948fba328
prabhusiva92 <sjprabhu91@gmail.com>
2021-12-31 12:24:26 +0530

    Folder Structure

---
+++ src/org/lib/LogIn.java
@@ -0,0 +1,7 @@
+package org.lib;
+
+public class LogIn {
+public static void main(String[] args) {
+	System.out.println("login page");
+}
+}
---
+++ src/org/test/Facebook.java
@@ -0,0 +1,8 @@
+package org.test;
+
+public class Facebook {
+	public static void main(String[] args) {
+		System.out.println("Welcome to GitHub");
+	}
+
+}
